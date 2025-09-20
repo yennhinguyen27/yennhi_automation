e8b454ea20abd1d210280b84f446287de9fbb6df
Yen Nhi Nguyen <yennhinguyen271099@gmail.com>
2025-09-20 19:48:34 +0700

    add file again

--- automation_framework/tests/google_test.py
+++ automation_framework/tests/google_test.py
@@ -8,16 +8,16 @@ def read_csv_data(file_path):
         return [row["keyword"] for row in reader]
 
 #call the function to read data for CSV
-test_data = read_csv_data('./data/data_test.csv')
+# test_data = read_csv_data('./data/data_test.csv')
 
 
 #test script 
-@pytest.mark.parametrize("keyword", test_data)
-def test_google_search(keyword):
-    driver = webdriver.Chrome()
-    driver.get("https://www.google.com")
-    search_box= driver.find_element("name","q")
-    search_box.send_keys(keyword)
-    search_box.submit()
+# @pytest.mark.parametrize("keyword", test_data)
+# def test_google_search(keyword):
+#     driver = webdriver.Chrome()
+#     driver.get("https://www.google.com")
+#     search_box= driver.find_element("name","q")
+#     search_box.send_keys(keyword)
+#     search_box.submit()
     
-    driver.quit()
+#     driver.quit()
